a29e2fd16aeb4663875a389f56803a2226169238
Illinois_Roadbuff <illinois_roadbuff@proton.me>
2026-05-06 00:12:52 -0500

    docs: try external docs and forgejo workflow
    Signed-off-by: Illinois_Roadbuff <illinois_roadbuff@proton.me>

---
+++ docs/nuxt.config.ts
@@ -0,0 +1,39 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  extends: ["shadcn-docs-nuxt"],
+  i18n: {
+    defaultLocale: "en",
+    locales: [
+      {
+        code: "en",
+        name: "English",
+        language: "en-US",
+      },
+    ],
+  },
+
+  vite: {
+    optimizeDeps: {
+      include: [
+        "debug", // CJS
+        "mermaid",
+        "@vue/devtools-core",
+        "@vue/devtools-kit",
+      ],
+    },
+  },
+  ssr: true,
+
+  nitro: {
+    preset: "static",
+  },
+
+  mdc: {
+    highlight: {
+      langs: ["ts", "js", "json", "bash", "html", "css", "luau"],
+    },
+  },
+
+  compatibilityDate: "2024-07-06",
+});
